e311eeba8679ec6bbd4288d47f5b3c3264e5a788
tom <beneda@ttc.marconi.cz>
2012-12-21 14:45:49 +0100

    empty classes

---
+++ SafeBet/src/cz/kojotak/icpc2012/SafeBet.java
@@ -0,0 +1,9 @@
+package cz.kojotak.icpc2012;
+
+public class SafeBet {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ SafeBet/src/cz/kojotak/icpc2012/SafeBetTest.java
@@ -0,0 +1,14 @@
+package cz.kojotak.icpc2012;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SafeBetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
